ff640f6679e6be427a31a7ae510497ce91bfc527
Ryan Icasiano <ricasiano@xyber.ph>
2012-08-04 20:43:41 +0800

    new file for config, reusable for libraries

---
+++ mainconfig.php
@@ -0,0 +1,5 @@
+<?php
+define('CONFIG_DATABASE_HOST', 'localhost');
+define('CONFIG_DATABASE_USER', 'root');
+define('CONFIG_DATABASE_PASSWORD', '');
+define('CONFIG_DATABASE_NAME', 'mca_presentation');
