ff2616ad1e8e33bcf33b5090c20b40ff57fff09c
Ryan Icasiano <ricasiano@xyber.ph>
2012-08-04 20:40:59 +0800

    new includes for config file, library

--- index.php
+++ index.php
@@ -13,8 +13,20 @@
  */
 define('WP_USE_THEMES', true);
 
-/** Loads the WordPress Environment and Template */
+/** Load the main config file, plugins will also access the DB*/
+require('mainconfig.php');
+
+/** Database connection and APIs */
+require('libraries/Database.php');
 require('libraries/APIRequest.php');
 require('libraries/MCA.php');
+require('libraries/ClientCatalog.php');
 $mca = new MCA();
+$client_catalog = new ClientCatalog();
+$client_catalog->host = CONFIG_DATABASE_HOST;
+$client_catalog->username = CONFIG_DATABASE_USER;
+$client_catalog->password = CONFIG_DATABASE_PASSWORD;
+$client_catalog->database_name = CONFIG_DATABASE_NAME;
+
+/** Loads the WordPress Environment and Template */
 require('./wp-blog-header.php');
