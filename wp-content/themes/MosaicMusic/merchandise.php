72c8571095fb2649581ed63ea3e172ad3c3dccc9
Ryan Icasiano <ricasiano@xyber.ph>
2012-08-04 23:26:18 +0800

    merchandise module

---
+++ wp-content/themes/MosaicMusic/merchandise.php
@@ -0,0 +1,31 @@
+<?php
+/*
+   Template Name: Merchandise
+ */
+//use the term id
+$client_catalog->identifier = 12;
+$client_catalog->taxonomy = 'catablog-terms';
+$result = $client_catalog->getItems();
+$upload_dir = wp_upload_dir();
+get_header();
+if (count($result) > 0) :
+    foreach($result as $content): 
+?>
+    <div class="new-album">
+        <a href="javascript:void(0)">
+            <div class="img-holder"><img src="<?php 
+            echo $upload_dir['baseurl'].'/catablog/originals/'.$content['image'];?>" /></div>
+            <h3><?php 
+            $title = explode(' - ', $content['post_title']);
+            echo trim($title[0]); ?></h3>
+            <h4><?php echo $content['post_content']; ?><br />PHP <?php echo $content['price'];?>
+</h4>
+                    </a>
+    </div>
+
+<?php
+    endforeach;
+endif;
+    ?>    <?php get_sidebars(); ?>
+    </div>
+    <?php get_footer(); ?>
