7d6ea1cf9c45e26fe7bd8bcdb4e10b289aeccb54
Ryan Icasiano <ricasiano@xyber.ph>
2012-08-06 10:27:37 +0800

    updated to use flash player instead of audio tag, since ie hates audio tags

--- popups/player.php
+++ popups/player.php
@@ -1,10 +1,19 @@
 <html>
 <head><title>Audio Player</title></head>
-<body>
-<audio controls="controls" autoplay="autoplay">
-    <source src="<?php echo urldecode($_GET['play_file']);?>" type="audio/mp3" />
-      Your browser does not support the audio player.
-      </audio> 
-</body>
-</html>
+<script type="text/javascript" src="http://localhost/mca-universal/popups/audio-player/audio-player.js"></script>  
+        <script type="text/javascript">  
+            AudioPlayer.setup("http://localhost/mca-universal/popups/audio-player/player.swf", {  
+                width: 290  
+            });  
+        </script>  
+  
+    </head>  
+    <body>  
+        <p id="audioplayer_1">Now Playing...</p>  
+        <script type="text/javascript">  
+        AudioPlayer.embed("audioplayer_1", {soundFile: "<?php echo urldecode($_GET['play_file']);?>"});  
+	</script>  
+    </body>  
+</html>  
+
 
